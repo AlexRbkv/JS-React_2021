0fae19e8bf4899a06bcfb1ba6ce2fc64cbfa6d32
AlxRbkv <wsro2012@mail.ru>
2021-10-24 14:10:32 +0400

    project

---
+++ script.js
@@ -0,0 +1,26 @@
+/*let money = prompt("Ваш бюджет на месяц?", 100);
+let time = prompt("Введите дату в формате YYYY-MM-DD", 100);
+
+let question1 = prompt("Введите обязательную статью расходов в этом месяце", "ответ");
+let question2 = prompt("Во сколько обойдется?",100);
+let question3 = prompt("Введите обязательную статью расходов в этом месяце", "ответ");
+let question4 = prompt("Во сколько обойдется?",100);
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        question1 : question2,
+        question3 : question4
+    },
+    optionalExpenses: {},
+    icome: [],
+    savings: false
+};
+
+alert("Ваш бюджет на 1 день " + money / 30);
+alert(question3);*/
+
+let a = [1,2];
+let b = [1,2];
+console.log(a == b);
